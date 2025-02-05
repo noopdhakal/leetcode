97c60e9729ae021f8f8a6d2c6ddba177aeb4845d
noopdhakal <dhakalanup015@gmail.com>
2025-02-05 07:54:11 +0545

    Using Prefix and Suffix Approach

--- python/products_of_array_238.py
+++ python/products_of_array_238.py
@@ -36,7 +36,27 @@ class Solution:
 # Time Complexity: o(n)
 # Space Complexity: O(1)
 
-# 3. 
+# 3. Prefix and Suffix Method
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n 
+        pref = [0] * n 
+        suff = [0] * n
+
+        pref[0] = suff[n-1] = 1 ## Prefix first 1 and suffix last 1
+        for i in range(1, n):
+            pref[i] = nums[i - 1] * pref[i-1] 
+            ## 1. nums[i-1] = nums[1-1] = nums[0] = 1, pref[1-1] = pref[0] = 1, 1*1 = 1
+            ## 2. nums[i-1] = nums[2-1] = nums[1] = 2, pref[2-1] = pref[1] = 1, 2*1 = 2
+            ## 2. nums[i-1] = nums[3-1] = nums[2] = 4, pref[3-1] = pref[2] = 2, 4*2 = 8
+        for i in range(n-2, -1, -1):
+            # i = 2, 1, 0
+            suff[i] = nums[i + 1] * suff[i+1] 
+        for i in range(n):
+            res[i] = pref[i] * suff[i]
+        return res
 
 d = Solution()
 print(d.productExceptSelf([1,2,4,6]))
