3f0765b88412e2290ff8336ddbc45698d51192b7
noopdhakal <dhakalanup015@gmail.com>
2025-02-05 08:04:52 +0545

    Optimal Solution

--- python/products_of_array_238.py
+++ python/products_of_array_238.py
@@ -22,15 +22,15 @@ from typing import List
 
 # 2. Division Method
 
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        for num in nums:
-            prod *= num
-        res = [0] * len(nums)
-        for i, c in enumerate(nums):
-            res[i] = prod // c
-        return res
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         prod = 1
+#         for num in nums:
+#             prod *= num
+#         res = [0] * len(nums)
+#         for i, c in enumerate(nums):
+#             res[i] = prod // c
+#         return res
       
 
 # Time Complexity: o(n)
@@ -38,25 +38,49 @@ class Solution:
 
 # 3. Prefix and Suffix Method
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         res = [0] * n 
+#         pref = [0] * n 
+#         suff = [0] * n
+
+#         pref[0] = suff[n-1] = 1 ## Prefix first 1 and suffix last 1
+#         for i in range(1, n):
+#             pref[i] = nums[i - 1] * pref[i-1] 
+#             ## 1. nums[i-1] = nums[1-1] = nums[0] = 1, pref[1-1] = pref[0] = 1, 1*1 = 1
+#             ## 2. nums[i-1] = nums[2-1] = nums[1] = 2, pref[2-1] = pref[1] = 1, 2*1 = 2
+#             ## 2. nums[i-1] = nums[3-1] = nums[2] = 4, pref[3-1] = pref[2] = 2, 4*2 = 8
+#         for i in range(n-2, -1, -1):
+#             # i = 2, 1, 0
+#             suff[i] = nums[i + 1] * suff[i+1] 
+#         for i in range(n):
+#             res[i] = pref[i] * suff[i]
+#         return res
+
+# Time Complexity: o(n)
+# Space Complexity: O(n)
+
+# 3. Prefix and Suffix Method (Optimal Solution)
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [0] * n 
-        pref = [0] * n 
-        suff = [0] * n
+        res = [1] * len(nums)
 
-        pref[0] = suff[n-1] = 1 ## Prefix first 1 and suffix last 1
-        for i in range(1, n):
-            pref[i] = nums[i - 1] * pref[i-1] 
-            ## 1. nums[i-1] = nums[1-1] = nums[0] = 1, pref[1-1] = pref[0] = 1, 1*1 = 1
-            ## 2. nums[i-1] = nums[2-1] = nums[1] = 2, pref[2-1] = pref[1] = 1, 2*1 = 2
-            ## 2. nums[i-1] = nums[3-1] = nums[2] = 4, pref[3-1] = pref[2] = 2, 4*2 = 8
-        for i in range(n-2, -1, -1):
-            # i = 2, 1, 0
-            suff[i] = nums[i + 1] * suff[i+1] 
-        for i in range(n):
-            res[i] = pref[i] * suff[i]
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
+
+# Time Complexity: o(n)
+# Space Complexity: O(1)
+
 
 d = Solution()
 print(d.productExceptSelf([1,2,4,6]))
