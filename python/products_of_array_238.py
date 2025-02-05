fd133f6733646529f46308607d0046b74d0ca758
noopdhakal <dhakalanup015@gmail.com>
2025-02-05 07:34:38 +0545

    Using Division Method

--- python/products_of_array_238.py
+++ python/products_of_array_238.py
@@ -2,24 +2,41 @@ from typing import List
 
 
 # 1. Brute Force Method
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
 
-        res = [0] * n
+#         res = [0] * n
 
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j] 
-            res[i] = prod
-        return res
+#         for i in range(n):
+#             prod = 1
+#             for j in range(n):
+#                 if i == j:
+#                     continue
+#                 prod *= nums[j] 
+#             res[i] = prod
+#         return res
     
-
 # Time Complexity: o(n2)
 # Space Complexity: O(1)
+
+# 2. Division Method
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod = 1
+        for num in nums:
+            prod *= num
+        res = [0] * len(nums)
+        for i, c in enumerate(nums):
+            res[i] = prod // c
+        return res
+      
+
+# Time Complexity: o(n)
+# Space Complexity: O(1)
+
+# 3. 
 
 d = Solution()
 print(d.productExceptSelf([1,2,4,6]))
