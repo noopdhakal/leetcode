0cc23a6154dc847f706c76aafb355cd45cbdff1b
noopdhakal <dhakalanup015@gmail.com>
2025-02-11 09:35:47 +0545

    Using integer sum

--- python/two_integer_sum.py
+++ python/two_integer_sum.py
@@ -1,7 +1,21 @@
+
+from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(len(numbers)):
-            for j in range(i + 1, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    return [i + 1, j + 1]
+        l, r = 0, len(numbers) -1
+
+        while l < r:
+
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l+1, r+1] ## as we need index start from 1 not 0 so returning values
         return []
+
+
+d = Solution()
+print(d.twoSum([1,2,3,4], target = 3))
