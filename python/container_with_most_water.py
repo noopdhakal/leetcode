91afb283cf05c8fab628fad790647a73e37bbf72
noopdhakal <dhakalanup015@gmail.com>
2025-02-14 08:05:25 +0545

    Brute Force method

---
+++ python/container_with_most_water.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+## Brute Force method
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        res = 0
+        
+        for l in range(len(heights)):
+            for r in range(l+1, len(heights)): # atleast 1 postion head of left
+                area = (r-l) * min(heights[l], heights[r])
+                res = max(res, area)
+
+        return res
+    
+
+
+    ## Time Complexity: (on2)
+    ## Space Complexity: O(1)
+
+d = Solution()
+print(d.maxArea([-1,0,1,2,-1,-4]))
