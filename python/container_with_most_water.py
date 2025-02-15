49d724e2470b285cc49a64dd8d26e1ae433ffd82
noopdhakal <dhakalanup015@gmail.com>
2025-02-15 14:34:53 +0545

    Container with most water

--- python/container_with_most_water.py
+++ python/container_with_most_water.py
@@ -19,5 +19,21 @@ class Solution:
     ## Time Complexity: (on2)
     ## Space Complexity: O(1)
 
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+# o(n)
+# O(1)
+
 d = Solution()
 print(d.maxArea([-1,0,1,2,-1,-4]))
