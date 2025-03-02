5cd7cfeb28b1f3460c8c86419741d3a00a9a14bc
noopdhakal <dhakalanup015@gmail.com>
2025-03-02 07:45:23 +0545

    Drop a column in sql

--- python/SQL/if_then_else.sql
+++ python/SQL/if_then_else.sql
@@ -3,3 +3,7 @@ select decode(sex, "M", "Male", "F", "Female", 'Unknown') from Employees;
 select decode(GREATEST(A, B), A, "A is greater than B", "B is greater than A") from dual;
 
 select * from Employees;
+
+Create table t2 as select <specific columns> from t1;
+   Drop table t1;
+   Rename t2 to t1;
